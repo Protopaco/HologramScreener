fefe622a32ef39106adca3fde36c88f74f885719
Paul Stevens <paul.stevens.developer@gmail.com>
2021-06-24 09:34:54 -0700

    cleans up if statements in evaluateSignal

--- THX1138.js
+++ THX1138.js
@@ -1,21 +1,15 @@
 
-let count = 1;
-
 const evaluateSignal = (count) => {
-    if (count % 3 === 0 && count % 5 === 0) {
-        return "SCAN_FOR_TOWERS";
-    } else if (count % 5 === 0) {
-        return "CHECK_CHANNEL_NOISE";
-    } else if (count % 3 === 0) {
-        return "CHECK_SIGNAL_STRENGTH";
-    }
+    if (count % 3 === 0 && count % 5 === 0) return "SCAN_FOR_TOWERS";
+    if (count % 5 === 0) return "CHECK_CHANNEL_NOISE";
+    if (count % 3 === 0) return "CHECK_SIGNAL_STRENGTH";
     return "PING"
 }
 
-do {
-    let signal = evaluateSignal(count);
-    console.log(`${count}: ${signal}`)
-    count++;
+let count = 1;
 
+do {
+    console.log(`${count}: ${evaluateSignal(count)}`)
+    count++;
 } while (count <= 100)
 
